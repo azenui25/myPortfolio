a38743ac5eefc8b77d391d2268518318153fcbe3
azenui25 <asanga.nicholas@yahoo.com>
2020-06-04 08:19:46 +0200

    add projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -15,14 +15,15 @@ class Projects extends Component {
         <div className="projects-grid">
           {/* Project 1 */}
           <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #1</CardTitle>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://townsquare.media/site/341/files/2013/04/Cutest-Dogs1.gif?w=980&q=75) center / cover'}} >Dog Memory</CardTitle>
             <CardText>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+            An app which behaves as an interactive game build using
+React, Redux, HTML CSS, Express and external API.
             </CardText>
             <CardActions border>
-              <Button colored>GitHub</Button>
-              <Button colored>CodePen</Button>
-              <Button colored>Live Demo</Button>
+            <a href="https://github.com/Remzi1993/dog-breed-memory" rel="noopener noreferrer" target="_blank"> <Button  colored>GitHub</Button></a> 
+            <a href="https://dog-memory.netlify.app/" rel="noopener noreferrer" target="_blank"> <Button  colored>App</Button></a> 
+              
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
               <IconButton name="share" />
@@ -66,7 +67,41 @@ class Projects extends Component {
       )
     } else if(this.state.activeTab === 1) {
       return (
-        <div><h1>This is geospatial</h1></div>
+        <div className="projects-grid">
+         <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+        <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://media0.giphy.com/media/133L9tE1zx87pC/source.gif) center / cover'}} >Erasmus Student Guide - Münster  
+A spatial story to your life experience, made by students for students!</CardTitle>
+        <CardText>
+        A spatial story to your life experience, made by students for students!
+        </CardText>
+        <CardActions border>
+        <a href="http://bit.ly/2OCZqXB" rel="noopener noreferrer" target="_blank"> <Button  colored>Story Map</Button></a> 
+        <a href="https://docs.google.com/document/d/1jqseo1fJj9AAMvdw-vLbAVufvcHh5k-KUDCHm4NPIWU/edit?usp=sharing" rel="noopener noreferrer" target="_blank"> <Button  colored>Documentation</Button></a> 
+          
+        </CardActions>
+        <CardMenu style={{color: '#fff'}}>
+          <IconButton name="share" />
+        </CardMenu>
+      </Card>
+
+      {/* project2 */}
+      <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+        <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://1.bp.blogspot.com/-v7z5MG6Y1Ss/XRixI58XnxI/AAAAAAAACTw/dChYs2bzlGglYdVQfgq3vbbFB0HymPNQQCLcBGAs/s1600/leafglezgif-3-9d22587083e9.gif) center / cover'}} >   Exploring the predictive power of daily weather data for rental-bike
+availability in The Netherlands</CardTitle>
+        <CardText>
+         The results show how weather affects Bike rental predictability
+        </CardText>
+        <CardActions border>
+        <a href="https://docs.google.com/document/d/1eCmn_E5XCYK7bnwbVJ4tFrmpbhLCrZiNlqmr217liCo/edit?usp=sharing" rel="noopener noreferrer" target="_blank"> <Button  colored>Paper</Button></a> 
+          
+        </CardActions>
+        <CardMenu style={{color: '#fff'}}>
+          <IconButton name="share" />
+        </CardMenu>
+      </Card>
+      
+      
+      </div>
       )
     } else if(this.state.activeTab === 2) {
       return (
